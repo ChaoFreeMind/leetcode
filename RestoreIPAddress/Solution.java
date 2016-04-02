14fc7c3c244ceea276d3af1f38300e151c9512a2
Chao <Chao@Huachaos-iMac.local>
2016-04-02 11:58:54 -0700

    RestoreIPAddress

---
+++ RestoreIPAddress/Solution.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        System.out.println(new Solution().restoreIpAddresses("25525511135"));
+    }
+    
+    public List<String> restoreIpAddresses(String s) {
+        
+        List<String> resList = new ArrayList<>();
+        
+        if(s.length() > 12 || s.length() < 4) return resList;
+        
+        backtrack(resList, s, new ArrayList<>(), 0);
+        
+        return resList;
+    }
+    
+    private void backtrack(List<String> resList, String str, List<String> ipList, int pos) {
+        
+        if(pos >= str.length()) {
+            if(ipList.size() == 4) {
+                resList.add(constructIp(ipList));
+            }
+            return;
+        }
+        
+        for(int i = 1; i <= 3; i++) {
+            if(pos + i > str.length()) break;
+            String s = str.substring(pos, pos + i);
+            if(Integer.parseInt(s) >= 0 && Integer.parseInt(s) <= 255) {
+                ipList.add(s);
+                backtrack(resList, str, ipList, pos + i);
+                ipList.remove(ipList.size() - 1);
+            }
+        }
+        
+    }
+    
+    private String constructIp(List<String> ipList) {
+        assert ipList.size() == 4;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < ipList.size(); i++) {
+            sb.append(ipList.get(i));
+            sb.append(".");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        
+        return sb.toString();
+    }
+    
+}
