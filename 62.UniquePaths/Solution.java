c062e9160538b56cdf9f84493137a5a89a6c2803
Chao <Chao@Huachaos-iMac.local>
2016-04-09 14:51:07 -0700

    4/9/2016 Afternoon

---
+++ 62.UniquePaths/Solution.java
@@ -0,0 +1,19 @@
+//Time O(m*n) Space O(m*n)
+    public int uniquePaths(int m, int n) {
+    	if(m == 0 || n == 0) return 0;
+        int[][] dp = new int[m][n]; //int[i][j] denotes the ways to get to i,j from 0,0.
+        
+        for(int i = 0; i < m; i++)
+        	dp[i][0] = 1;
+        
+        for(int j = 0; j < n; j++)
+        	dp[0][j] = 1;
+        
+        for(int i = 1; i < m; i++) {
+        	for(int j = 1; j < n; j++) {
+        		dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        	}
+        }
+        
+        return dp[m-1][n-1];
+    }
