da411dd883f19074940ddd2ab44d8fa30fab7983
MiniPekka <chaoye917@gmail.com>
2017-05-06 16:06:11 -0700

    5/6/2017 Afternoon

--- Java/535.EncodeAndDecodeTinyURL/Codec.java
+++ Java/535.EncodeAndDecodeTinyURL/Codec.java
@@ -1,13 +1,25 @@
+// Index counter solution:
+// Use a counter as index to store the incoming longUrls.
+// The time complexity would be O(1) and space would be O(n) where n is the total
+// length longUrls that has been stored.
+// The downside of this method is that the number of longUrl is limited by the limit of long type in
+// java.
+
 public class Codec {
+
+    private long idx = LONG.MIN_VALUE;
+    Map<Long, String> map = new HashMap<>();
 
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        
+        idx++;
+        map.put(idx, longUrl);
+        return String.valueOf(idx);
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        
+        return map.get(Long.parseLong(shortUrl));
     }
 }
 
