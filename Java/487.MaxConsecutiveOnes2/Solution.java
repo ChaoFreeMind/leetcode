b7d2d3513bdbfdc914ec61442b163454e40436d8
MiniPekka <chaoye917@gmail.com>
2017-05-07 10:39:16 -0700

    Clean up

--- Java/487.MaxConsecutiveOnes2/Solution.java
+++ Java/487.MaxConsecutiveOnes2/Solution.java
@@ -16,7 +16,55 @@
 // efficiently?
 
 public class Solution {
+
+    // Populate two dp arrays: start and end.
+    // When we see a 0, the number of consecutive 1s when we flip it is equal to end[i-1] + start[i+1]
     public int findMaxConsecutiveOnes(int[] nums) {
-        
+        // end[i]: Number of 1s ending at index i
+        int[] end = new int[nums.length];
+        int[] start = new int[nums.length];
+        int ones = 0;
+        int maxOriginal = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                ones++;
+                maxOriginal = Math.max(maxOriginal, ones);
+                end[i] = ones;
+            } else {
+                ones = 0;
+            }
+        }
+
+        ones = 0;
+        // start[i]: Number of 1s starting from i
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] == 1) {
+                ones++;
+                start[i] = ones;
+            } else {
+                ones = 0;
+            }
+        }
+
+        int max = 0;
+        // In case of all 1s, return the number of 1s.
+        if (maxOriginal == nums.length) return maxOriginal;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                // If we flip the current 0...
+                int localMax = 1;
+                if (i - 1 >= 0) {
+                    localMax += end[i - 1];
+                }
+
+                if (i + 1 < nums.length) {
+                    localMax += start[i + 1];
+                }
+                max = Math.max(localMax, max);
+            }
+        }
+
+        return max;
     }
 }
